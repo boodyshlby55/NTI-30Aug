d1bc0b55fc2c52211b3a389840a433777e6390df
boodyshlby55 <boodyshlby55@gmail.com>
2024-09-27 20:06:36 +0300

    session16

--- dashboard/src/app/orders/orders.component.ts
+++ dashboard/src/app/orders/orders.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { OrdersService } from '../services/orders.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss'
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  orders: any[] = [];
+  productsImage: string = '';
+  pagination: any = {};
+  page: number = 1;
+  limit: number = 50;
+  search: string = '';
+  sort: string = '-createdAt';
+  constructor(private _AuthService: AuthService, private _OrdersService: OrdersService) { }
 
+  getAllOrders() {
+    this._OrdersService.getAll(this.limit, this.page, this.sort, this.search).subscribe({
+      next: (res) => {
+        this.orders = res.data;
+        this.pagination = res.pagination
+      },
+      error: (err) => { }
+    })
+  }
+
+  updatePaid(orderId: string) {
+    this._OrdersService.updatePaid(orderId).subscribe({
+      next: (res) => {
+        this.getAllOrders();
+        alert('order paid successfully');
+      },
+      error: (err) => { }
+    })
+  }
+
+  updateDelivered(orderId: string) {
+    this._OrdersService.updateDelivered(orderId).subscribe({
+      next: (res) => {
+        this.getAllOrders();
+        alert('order delivered successfully');
+      },
+      error: (err) => { }
+    })
+  }
+
+  changePage(page: number) {
+    this.page = page;
+    this.getAllOrders();
+  }
+
+  searchData(search: string) {
+    this.search = search;
+    this.getAllOrders();
+  }
+
+  ngOnInit(): void {
+    this._AuthService.checkToken();
+    this.productsImage = this._OrdersService.productsImage;
+    this.getAllOrders();
+  }
 }
--- dashboard/src/app/services/coupons.service.ts
+++ dashboard/src/app/services/coupons.service.ts
@@ -1,9 +1,73 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GlobalService } from './global.service';
+import { Observable } from 'rxjs';
+import Cookies from 'js-cookie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CouponsService {
+  private baseUrl: string = '';
+  private couponsRoute: string = '';
+  private apiKey: string = '';
+  constructor(private _HttpClient: HttpClient, private _GlobalService: GlobalService) {
+    this.baseUrl = _GlobalService.baseUrl;
+    this.couponsRoute = _GlobalService.couponsRoute;
+    this.apiKey = _GlobalService.apiKey;
+  }
 
-  constructor() { }
+  getAll(limit: number = 50, page: number = 1, sort: string = '-expireTime', search: string = ''): Observable<any> {
+    return this._HttpClient.get(`${this.baseUrl}${this.couponsRoute}?limit=${limit}&page=${page}&sort=${sort}&search=${search}`, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('user')}`,
+        'X-API-KEY': `${this.apiKey}`
+      },
+      withCredentials: true
+    })
+  }
+
+  getOne(couponId: string): Observable<any> {
+    return this._HttpClient.get(`${this.baseUrl}${this.couponsRoute}/${couponId}`, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('user')}`,
+        'X-API-KEY': `${this.apiKey}`
+      },
+      withCredentials: true
+    })
+  }
+
+  createOne(formData: any): Observable<any> {
+    return this._HttpClient.post(`${this.baseUrl}${this.couponsRoute}`, formData, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('user')}`,
+        'X-API-KEY': `${this.apiKey}`,
+        'X-CSRF-Token': `${Cookies.get('cookies')}`
+      },
+      withCredentials: true
+    })
+  }
+
+  updateOne(couponId: string, formData: any): Observable<any> {
+    return this._HttpClient.put(`${this.baseUrl}${this.couponsRoute}/${couponId}`, formData, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('user')}`,
+        'X-API-KEY': `${this.apiKey}`,
+        'X-CSRF-Token': `${Cookies.get('cookies')}`
+      },
+      withCredentials: true
+    })
+  }
+
+  deleteOne(couponId: string): Observable<any> {
+    return this._HttpClient.delete(`${this.baseUrl}${this.couponsRoute}/${couponId}`, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('user')}`,
+        'X-API-KEY': `${this.apiKey}`,
+        'X-CSRF-Token': `${Cookies.get('cookies')}`
+      },
+      withCredentials: true
+    })
+  }
+
 }
